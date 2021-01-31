10d6153eb9e5ff22254dbb41f1d853853ca50619
shaqil10 <58369573+shaqil10@users.noreply.github.com>
2021-01-30 20:45:47 -0800

    added midi_convert stuff and data structuring

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 import music21
+import glob
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from music21 import converter, instrument, note, chord
+import midi_convert
+    
